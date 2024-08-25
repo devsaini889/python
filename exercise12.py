3011e0b8a6cbf5b437849909c97184b4d6bb9d66
devsaini889 <ds0752607@gmail.com>
2024-08-25 09:18:15 +0530

    dunder dunction in Oop

---
+++ exercise12.py
@@ -0,0 +1,14 @@
+# A program to understand the dunder function 
+
+class order:
+    def __init__(self,item,price):
+        self.item = item
+        self.price = price
+
+    def __gt__(self,odr2):
+        return self.price > odr2.price
+
+odr1 = order("chips",20)
+odr2 = order("tea",15)
+
+print(odr1 > odr2) 
