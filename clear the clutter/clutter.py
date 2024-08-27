2d3e148b03084b7320584cafea926034d73e0ac7
devsaini889 <ds0752607@gmail.com>
2024-08-27 22:44:22 +0530

    clear the clutter

---
+++ clear the clutter/clutter.py
@@ -0,0 +1,9 @@
+import os
+
+files = os.listdir("clear the clutter")
+i=0
+for file in files:
+    if file.endswith(".jpg"):
+        print(file)
+        os.rename(f"clear the clutter/{file}", f"clear the clutter/{i}.jpg")
+        i=i+1
