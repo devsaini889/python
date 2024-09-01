3d098e5bb0d52bf23c4e95c5c97809786c90d65f
devsaini889 <ds0752607@gmail.com>
2024-09-01 11:38:25 +0530

    Tkinter GUI

---
+++ Tkinter/tk_a.py
@@ -0,0 +1,13 @@
+
+
+from tkinter import *
+
+dev_root = Tk()
+dev_root.geometry("666x555")
+dev_root.minsize(666,222)
+dev_root.maxsize(700,900)
+
+dev_root = Label(text='Hello everyone! This is my first puthon GUI')
+dev_root.pack()
+
+dev_root.mainloop()
