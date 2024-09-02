a00d989ab556caa77f0435a97a3c46dea8870dfa
devsaini889 <ds0752607@gmail.com>
2024-09-02 16:33:24 +0530

    Tkinter GUI

---
+++ Tkinter/tk_c.py
@@ -0,0 +1,13 @@
+
+
+from tkinter import *
+
+root = Tk()
+root.geometry("500x400")
+root.title('Filling text')
+
+text = Label(text='Ready' , bg ='red')
+text.pack(side='bottom' , fill='x')
+
+root.mainloop()
+
