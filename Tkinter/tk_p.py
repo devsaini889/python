6821858eed8cdacec0e15a4ca3fb8cf67453ef54
devsaini889 <ds0752607@gmail.com>
2024-09-09 17:58:04 +0530

    Tkinter GUI

--- Tkinter/tk_p.py
+++ Tkinter/tk_p.py
@@ -1,15 +1,23 @@
 from tkinter import *
 
 root = Tk()
+
 def update():
     print("updating geometry")
     root.geometry(f"{width.get()}x{height.get()}")
+
 root.geometry("500x400")
+
 width = StringVar()
 height = StringVar()
 
-Entry(root , textvariable= width).pack()
-Entry(root , textvariable=  height).pack()
-Button(root ,text= 'update', command= update).pack()
+# Create and pack the labels and entry widgets
+Label(root, text="Width:").pack()
+Entry(root, textvariable=width).pack()
+
+Label(root, text="Height:").pack()
+Entry(root, textvariable=height).pack()
+
+Button(root, text='Update', command=update).pack(pady=10)
 
 root.mainloop()
